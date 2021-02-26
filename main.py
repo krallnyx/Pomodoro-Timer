5e047e518e0dc3d563ed8b77706df6ef1767c247
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-02-26 16:08:18 +0000

    #3 added other elements of the layout

--- main.py
+++ main.py
@@ -25,6 +25,18 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_label = Label(text="✔", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
 
 window.mainloop()
