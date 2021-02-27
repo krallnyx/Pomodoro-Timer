43778bd1c594c6f81a7a58bfcd22477a91a1fd70
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-02-27 10:35:15 +0000

    #5 fixing the 1 digit seconds

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def start_timer():
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=f"{math.floor(count / 60)}:{count % 60}")
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{math.floor(count / 60)}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count-1)
 
